149b304aa395728d3f4e3678cd0acdf850ef9758
Kathy <kathylam2001@gmail.com>
2024-07-23 02:42:04 -0400

    Update Calculator.java
    An updated version of my calculator from my college classes will make a more advance version eventually.

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,57 @@
+/*An updated version of my calculator from my college classes will make a more advance version eventually.*/
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Calculator {
-    public static void main(String args[]) {
-      int num1 = 0;
-      int num2 = 0;
-      char operator;
+    public static void main(String[] args) {
+        double num1 = 0;
+        double num2 = 0;
+        char operator;
+        double answer = 0.0;
+
+        Scanner scanObject = new Scanner(System.in);
+
+        try {
+            System.out.println("Please enter first number: ");
+            num1 = scanObject.nextDouble();
+
+            System.out.println("Please enter second number: ");
+            num2 = scanObject.nextDouble();
+
+            System.out.println("What operation? ");
+            operator = scanObject.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    answer = num1 + num2;
+                    break;
+                case '-':
+                    answer = num1 - num2;
+                    break;
+                case '*':
+                    answer = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        answer = num1 / num2;
+                    } else {
+                        System.out.println("Error! Division by zero is not allowed.");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Error! Invalid operator.");
+                    return;
+            }
+
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + answer);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error! Invalid input. Please enter numerical values.");
+        } finally {
+            scanObject.close();
+        }
     }
 }
+
